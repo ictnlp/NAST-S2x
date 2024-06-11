54be5aa697b3487529a35eaf47e8779a50185aea
Zhengrui Ma <43530347+Paulmzr@users.noreply.github.com>
2024-06-12 02:19:43 +0800

    Update offline_s2u_infer.sh

--- test_scripts/offline_s2u_infer.sh
+++ test_scripts/offline_s2u_infer.sh
@@ -4,7 +4,7 @@ MAIN=chunk_size
 OUT_ROOT=path_to_your_output
 NAST_DIR=path_to_nast_dir
 
-python nast/cli/generate_ctc_unit.py ${CVSS_ROOT}/fr-en/fbank2unit \
+python nast/cli/generate_ctc_unit.py ${CVSS_ROOT} \
   --user-dir ${NAST_DIR} \
   --config-yaml config.yaml --gen-subset test --task nat_speech_to_unit_ctc_modified --src-upsample-ratio 1 --unit-size 2 --hidden-upsample-ratio 6 --main-context ${MAIN} --right-context ${MAIN} \
   --path ${PATH} \
